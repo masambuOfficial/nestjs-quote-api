4769dfd319211873f1b650445eddfd21e4595548
Yesuanjagala Carsten <yesuanjagalacarsten@Yesuanjagalas-MacBook-Pro.local>
2024-10-20 12:50:21 +0300

    Delete the Authors resources, and modify the quotes controller to have end points

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuotesModule } from './quotes/quotes.module';
-import { AuthorsModule } from './authors/authors.module';
 
 @Module({
-  imports: [QuotesModule, AuthorsModule],
+  imports: [ QuotesModule ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/quotes/quotes.controller.ts
+++ src/quotes/quotes.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { QuotesService } from './quotes.service';
 
 @Controller('quotes')
-export class QuotesController {}
+export class QuotesController {
+    constructor(private readonly quotesService: QuotesService) {}
+    
+    // GET /quotes --> []
+    @Get()
+    getQuotes(){
+        return [];
+    }
+
+    // GET /quotes/:id --> { ..... }
+    @Get(':id')
+    getOneQuotes(){
+        return [];
+    }
+    // POST /quotes/:id --> { .... }
+    // PUT /quotes/:id --> { ..... }
+    // DELETE /quotes/:id
+}
+
+
